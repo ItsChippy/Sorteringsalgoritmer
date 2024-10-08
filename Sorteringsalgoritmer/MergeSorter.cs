f13da9f28097e6fd8456b98cc5f172079a09dc83
Logan <142813655+ItsChippy@users.noreply.github.com>
2024-10-08 18:02:01 +0200

    Add project files.

---
+++ Sorteringsalgoritmer/MergeSorter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorteringsalgoritmer
+{
+    internal class MergeSorter
+    {
+
+        public void Sort(int[] a)
+        {
+            MergeSort(a, 0, a.Length);
+        }
+
+        private void MergeSort(int[] a, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = (left + right) / 2;
+
+                MergeSort(a, left, mid);
+
+                MergeSort(a, mid + 1, right);
+
+                Merge(a, left, mid, right);
+            }
+        }
+
+        private void Merge(int[] a, int left, int mid, int right)
+        {
+            int leftSize = mid - left + 1;
+            int rightSize = right - mid;
+
+            int[] leftArray = new int[leftSize];
+            int[] rightArray = new int[rightSize];
+
+            for (int i = 0; i < leftSize; ++i)
+            {
+                leftArray[i] = a[left + i];
+            }
+            for (int j = 0; j < rightSize; ++j)
+            {
+                rightArray[j] = a[mid + 1 + j];
+            }
+
+            int leftPointer = 0, rightPointer = 0;
+            int mergedPointer = left;
+
+            while (leftPointer < leftSize && rightPointer < rightSize)
+            {
+                if (leftArray[leftPointer] <= rightArray[rightPointer])
+                {
+                    a[mergedPointer] = leftArray[leftPointer];
+                    leftPointer++;
+                }
+                else
+                {
+                    a[mergedPointer] = rightArray[rightPointer];
+                    rightPointer++;
+                }
+                mergedPointer++;
+            }
+
+            while (leftPointer < leftSize)
+            {
+                a[mergedPointer] = leftArray[leftPointer];
+                leftPointer++;
+                mergedPointer++;
+            }
+
+            while (rightPointer < rightSize)
+            {
+                a[mergedPointer] = rightArray[rightPointer];
+                rightPointer++;
+                mergedPointer++;
+            }
+        }
+    }
+}
---
+++ Sorteringsalgoritmer/Quicksorter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorteringsalgoritmer
+{
+    internal class Quicksorter
+    {
+
+        public void Sort(int[] a)
+        {
+            QuickSort(a, 0, a.Length);
+        }
+
+        private void QuickSort(int[] a, int low, int high)
+        {
+            if (low < high)
+            {
+                int p = Partition(a, low, high);
+
+                QuickSort(a, low, p - 1);
+
+                QuickSort(a, p + 1, high);
+            }
+        }
+
+        private int Partition(int[] a, int low, int high)
+        {
+            int pivot = a[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (a[j] == pivot)
+                {
+                    i++;
+
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+
+            int temp1 = a[i + 1];
+            a[i + 1] = a[high];
+            a[high] = temp1;
+            return i + 1;
+        }
+    }
+}
